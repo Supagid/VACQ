8a07fe62d9911ddfad0ca636ac1792cfae11ddff
Supagid <6470285621@student.chula.ac.th>
2022-03-19 11:44:28 +0700

    Add Relationships and Authorization of Appointment

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,10 +1,65 @@
+const { start } = require('repl');
 const Hospital = require('../models/Hospital');
 //@desc    Get all hospitals
 //@route   GET /api/v1/hospitals
 //@access  Public
 exports.getHospitals= async (req,res,next)=>{
+    let query;
+
+    //Copy req.query
+    const reqQuery= {...req.query};
+
+    //Fields to exclude
+    const removeFields=['select','sort','page','limit'];
+
+    //Loop over remove fields and delete them from reqQuery
+    removeFields.forEach(param=>delete reqQuery[param]);
+    console.log(reqQuery);
+
+    //Create query string
+    let queryStr=JSON.stringify(reqQuery);
+    // let queryStr=JSON.stringify(req.query);
+    queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=>`$${match}`);
+    query=Hospital.find(JSON.parse(queryStr)).populate('appointments');
+
+    //Select Fields
+    if(req.query.select) {
+        const fields=req.query.select.split(',').join(' ');
+        query=query.select(fields);
+    }
+
+    //Sort
+    if(req.query.sort) {
+        const sortBy=req.query.sort.split(',').join(' ');
+        query=query.sort(sortBy);
+    } else {
+        query=query.sort('-createdAt');
+    }
+
+    //Pagination
+    const page=parseInt(req.query.page,10)||1;
+    const limit=parseInt(req.query.limit,10)||25;
+    const startIndex=(page-1)*limit;
+    const endIndex=page*limit;
+
     try {
-        const hospitals = await Hospital.find();
+        const total=await Hospital.countDocuments();
+        query=query.skip(startIndex).limit(limit);
+
+        //Execute query
+        const hospitals = await query
+        // const hospitals = await Hospital.find(req.query);
+        // console.log(req.query);
+
+        //Pagination result
+        const Pagination={};
+        if(endIndex<total){
+            Pagination.next={page:page+1,limit}
+        }
+        if(startIndex>0){
+            Pagination.prev={page:page-1,limit}
+        }
+
         res.status(200).json({success:true, count:hospitals.length, data:hospitals});
     } catch(err) {
         res.status(400).json({success:false});
@@ -63,11 +118,13 @@ exports.updateHospital= async(req,res,next)=>{
 //@access  Private
 exports.deleteHospital= async (req,res,next)=>{
     try {
-        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        // const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        const hospital = await Hospital.findById(req.params.id);
 
         if(!hospital){
             return res.status(400).json({success:false});
         }
+        hospital.remove();
         res.status(200).json({success:true, data:{}});
     } catch(err) {
         res.status(400).json({success:false});

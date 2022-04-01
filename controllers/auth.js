90d6ba384d654c3a58c37cede0c3916d4111309c
Supagid <6470285621@student.chula.ac.th>
2022-04-01 23:41:33 +0700

    Add API Security

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,30 +30,32 @@ exports.register = async (req,res,next) => {
 //@desc    Login user
 //@route   POST /api/v1/auth/login
 //@access  Public
-exports.login = async (req,res,nect) => {
-    const {email, password} = req.body;
-    //Validate email & password
-    if(!email || !password){
-        return res.status(400).json({success:false,
-        msg:'Please provide an email and password'});
+exports.login = async (req,res,next) => {
+    try{
+        const {email, password} = req.body;
+        //Validate email & password
+        if(!email || !password){
+            return res.status(400).json({success:false,
+            msg:'Please provide an email and password'});
+        }
+        //Check for user
+        const user = await
+        User .findOne ({email}).select('+password');
+        if(!user){
+            return res.status(400).json({success:false,
+            msg:'invalid credentials'});
+        }
+        //Check if password matches
+        const isMatch = await user.matchPassword(password);
+        if(!isMatch){
+            return res.status(400).json({success:false,
+            msg:'invalid credentials'});
+        }
+        //Create token
+        sendTokenResponse(user,200,res);
+    }catch(err){
+        return res.status(401).json({success:false, msg:'Cannot convert email or password to string'});
     }
-    //Check for user
-    const user = await
-    User .findOne ({email}).select('+password');
-    if(!user){
-        return res.status(400).json({success:false,
-        msg:'invalid credentials'});
-    }
-    //Check if password matches
-    const isMatch = await user.matchPassword(password);
-    if(!isMatch){
-        return res.status(400).json({success:false,
-        msg:'invalid credentials'});
-    }
-    //Create token
-    //const token = user.getSignedJwtToken();
-    //res.status(200).json({success:true,token});
-    sendTokenResponse(user,200,res);
 };
 
 //Get token from model, Create cookie and send response
@@ -71,6 +73,20 @@ const sendTokenResponse=(user, statusCode, res) =>{
         success: true,
         token
     })
+};
+
+//@desc Log user out / clear cookie
+//@route GET /api/v1/auth/logout
+//@access Private
+exports.logout=async(req,res,next)=>{
+    res.cookie('token','none',{
+        expires: new Date(Date.now()+ 10*1000),
+        httpOnly:true
+    });
+    res.status(200).json({
+        success:true,
+        data:{}
+    });
 };
 
 //At the end of file
